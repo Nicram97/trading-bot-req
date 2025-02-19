dcecc8667ab7a83a70ad2ea4eeae11c2c6b65b83
Nicram97 <marcin952@gmail.com>
2025-02-19 15:55:48 +0100

    chore: update incode docs for GetHistoricalDataDto;

--- src/api/dto/getHistoricalData.dto.ts
+++ src/api/dto/getHistoricalData.dto.ts
@@ -4,18 +4,18 @@ import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
 export class GetHistoricalDataDto {
   @IsString()
   @IsNotEmpty()
-  symbol: string;
+  symbol: string; // Represents symbol of exchanged pair for exmaple BTCUSDT => BASE|QUOTE => 'WhatWasBought|WithWhatWeBought
 
   @IsNumber()
   @Min(0)
-  startTime: number;
+  startTime: number; // start of time aggregation represented by timestamp in milliseconds
 
   @IsNumber()
   @Min(0)
-  endTime: number;
+  endTime: number; // end time of aggregation represented by timestamp in milliseconds
 
   @IsNumber()
   @Min(1)
   @Max(1000)
-  limit = 500;
+  limit = 500; // how many trades to show default 500, max 1000 has to be positive number
 }
